8bfa618b9dba9f5c79d1b90cc3c3cc2a55f2e5de
JezisVedToJeMojzis <mojzissamuel@gmail.com>
2023-10-19 10:22:38 +0200

    fixed js code for pointers and added one more

---
+++ programs/project/pointer1.js
@@ -0,0 +1,10 @@
+function pointer() {
+    var x = 5;       
+    var y;          
+
+    var *ptr = &x;  //Assign the address of x to the pointer ptr
+
+    y = *ptr;  // Use the pointer to access the value of x and assign it to y
+
+    assert(y == 5)
+}
